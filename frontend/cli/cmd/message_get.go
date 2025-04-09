7716c004a7da706d50044c84323d7d5e32a0ef6b
Thomas Schubart <24721048+Furisto@users.noreply.github.com>
2025-04-09 14:52:55 +0200

    Message Commands (#13)

---
+++ frontend/cli/cmd/message_get.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"connectrpc.com/connect"
+	v1 "github.com/furisto/construct/api/go/v1"
+	"github.com/spf13/cobra"
+)
+
+var messageGetCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get a message",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client := getClient()
+
+		resp, err := client.Message().GetMessage(cmd.Context(), &connect.Request[v1.GetMessageRequest]{
+			Msg: &v1.GetMessageRequest{Id: args[0]},
+		})
+
+		if err != nil {
+			return err
+		}
+
+		return DisplayResources([]*DisplayMessage{ConvertMessageToDisplay(resp.Msg.Message)}, formatOptions.Output)
+	},
+}
+
+func init() {
+	addFormatOptions(messageGetCmd)
+	messageCmd.AddCommand(messageGetCmd)
+}
