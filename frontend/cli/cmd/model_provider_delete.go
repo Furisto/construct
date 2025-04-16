3abd60a1498956cf5acc5efd0653c39892af3093
Thomas Schubart <24721048+Furisto@users.noreply.github.com>
2025-04-16 16:12:27 +0200

    Delete models of model provider

--- frontend/cli/cmd/model_provider_delete.go
+++ frontend/cli/cmd/model_provider_delete.go
@@ -14,7 +14,37 @@ var modelProviderDeleteCmd = &cobra.Command{
 		client := getClient()
 
 		for _, id := range args {
-			_, err := client.ModelProvider().DeleteModelProvider(cmd.Context(), &connect.Request[v1.DeleteModelProviderRequest]{
+			resp, err := client.ModelProvider().GetModelProvider(cmd.Context(), &connect.Request[v1.GetModelProviderRequest]{
+				Msg: &v1.GetModelProviderRequest{Id: id},
+			})
+
+			if err != nil {
+				return err
+			}
+
+			models, err := client.Model().ListModels(cmd.Context(), &connect.Request[v1.ListModelsRequest]{
+				Msg: &v1.ListModelsRequest{
+					Filter: &v1.ListModelsRequest_Filter{
+						ModelProviderId: &resp.Msg.ModelProvider.Id,
+					},
+				},
+			})
+
+			if err != nil {
+				return err
+			}
+
+			for _, model := range models.Msg.Models {
+				_, err = client.Model().DeleteModel(cmd.Context(), &connect.Request[v1.DeleteModelRequest]{
+					Msg: &v1.DeleteModelRequest{Id: model.Id},
+				})
+
+				if err != nil {
+					return err
+				}
+			}
+			
+			_, err = client.ModelProvider().DeleteModelProvider(cmd.Context(), &connect.Request[v1.DeleteModelProviderRequest]{
 				Msg: &v1.DeleteModelProviderRequest{Id: id},
 			})
 
