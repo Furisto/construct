83372d2e486621f6fff858970c7d81250de19521
Thomas Schubart <24721048+Furisto@users.noreply.github.com>
2025-06-15 16:31:29 +0200

    Symbols

---
+++ frontend/cli/pkg/terminal/symbols.go
@@ -0,0 +1,81 @@
+package terminal
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	infoSymbolStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("33")).
+			Bold(true).
+			SetString("ⓘ")
+
+	errorSymbolStyle = lipgloss.NewStyle().
+				SetString("❌")
+
+	warningSymbolStyle = lipgloss.NewStyle().
+				SetString("⚠️")
+
+	successSymbolStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("10")).
+				Bold(true).
+				SetString("✔")
+
+	attentionSymbolStyle = lipgloss.NewStyle().
+				SetString("❗")
+
+	questionSymbolStyle = lipgloss.NewStyle().
+				SetString("❓")
+
+	actionSymbolStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("39")).
+				SetString("▶")
+
+	linkSymbolStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("75")).
+			SetString("→")
+
+	docsStyle = lipgloss.NewStyle().
+			SetString("📚")
+
+	communitySymbolStyle = lipgloss.NewStyle().
+				SetString("💬")
+
+	bugSymbolStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("208")).
+			SetString("⚑")
+)
+
+var (
+
+	// InfoSymbol (ⓘ)
+	InfoSymbol = infoSymbolStyle.String()
+
+	// WarningSymbol (⚠️)
+	WarningSymbol = warningSymbolStyle.String()
+
+	// ErrorSymbol (❌)
+	ErrorSymbol = errorSymbolStyle.String()
+
+	// SuccessSymbol (✔)
+	SuccessSymbol = successSymbolStyle.String()
+
+	// AttentionSymbol (❗)
+	AttentionSymbol = attentionSymbolStyle.String()
+
+	// QuestionSymbol (❓)
+	QuestionSymbol = questionSymbolStyle.String()
+
+	// ActionSymbol (▶)
+	ActionSymbol = actionSymbolStyle.String()
+
+	// LinkSymbol (→)
+	LinkSymbol = linkSymbolStyle.String()
+
+	// DocsSymbol (📚)
+	DocsSymbol = docsStyle.String()
+
+	// CommunitySymbol (💬)
+	CommunitySymbol = communitySymbolStyle.String()
+
+	// BugSymbol (⚑)
+	BugSymbol = bugSymbolStyle.String()
+)
